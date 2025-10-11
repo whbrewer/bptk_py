4c2c0acbe073edd0cd76b9fd065f00b1fbf2748d
Oliver Grasl <oliver.grasl@transentis.com>
2025-10-11 12:55:39 +0200

    remove obsolete script

--- scripts/push_docs_s3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# build the docs first with "make html"
-# aws access controls need to be set - call "aws configure" if these are not set
-# credentials of bptk-website user in production environment are needed
-aws s3 cp ../_build/html s3://bptk.transentis.com/en/latest --recursive --acl  public-read --profile bptk-website
